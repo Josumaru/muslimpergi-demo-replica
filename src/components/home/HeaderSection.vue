6079b43447bdcffeab4544a10cb6c5e08595a964
Fad <fadhilerdyaqasmal@gmail.com>
2025-04-28 14:37:44 +0700

    feat: added headder section

--- src/components/home/HeaderSection.vue
+++ src/components/home/HeaderSection.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="header">
-        Header
+        <div class="header__content">
+            <div class="relative">
+                <img src="@/assets/HeaderComponent/header-image.png" alt="Header Image" class="header__image w-full h-auto rounded-b-2xl">
+                <div class="absolute inset-0 bg-gradient-to-t from-[#1D421A]/100 rounded-b-2xl"></div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-</script>
 
-<style lang="css" scoped>
-.header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-</style>
+
+<script>
+
+</script>
